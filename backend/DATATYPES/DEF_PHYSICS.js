bd02f19c1327daa6a4a847e59fb5e4d777b81af1
Reu Zuidema <22671898+reumarks@users.noreply.github.com>
2022-02-15 00:05:04 -0500

    tuned fish movement

--- backend/DATATYPES/DEF_PHYSICS.js
+++ backend/DATATYPES/DEF_PHYSICS.js
@@ -8,44 +8,50 @@ let Physics = class {
                 player.physics.x = newPosX;
             }else{
                 player.physics.vx *= -0.9;
-                player.physics.vr = player.physics.vy * (player.physics.vx > 0 ? 1 : -1) * 0.02;
+                player.physics.vr = player.physics.vy * (player.physics.vx > 0 ? 1 : -1) * 0.015;
             }
             if (gameMap.getCurrentTile(player.physics.x, newPosY) !== "#") {
                 player.physics.y = newPosY;
             }else{
                 player.physics.vy *= -0.9;
-                player.physics.vr = player.physics.vx * (player.physics.vy > 0 ? -1 : 1) * 0.02;
+
+                if(Math.abs(player.physics.vx) > 0.7){
+                    player.physics.vr = player.physics.vx * (player.physics.vy > 0 ? -0.5 : 1) * 0.02;
+                }else{
+                    player.physics.vr = player.physics.vx * (player.physics.vy > 0 ? 1 : -1) * 0.005;
+                }
+                
+                if(Math.abs(player.physics.vr) < 0.2){
+                    if(player.physics.r < Math.PI/2){
+                        player.physics.r *= 0.6;
+                    }else if(player.physics.r > Math.PI * 3/2){
+                        player.physics.r = 2 * Math.PI + (player.physics.r - 2 * Math.PI) * 0.6;
+                    }else{
+                        player.physics.r = Math.PI + (player.physics.r - Math.PI) * 0.6;
+                    }
+                }
             }
 
             player.physics.vy += 0.06;
-            player.physics.vy *= 0.999;
-            player.physics.vx *= 0.995;
+            player.physics.vy *= 0.998;
+            player.physics.vx *= 0.997;
             player.physics.vr *= 0.995;
 
             player.physics.r += player.physics.vr;
 
             player.physics.r = ((Math.PI * 4) + player.physics.r) % (Math.PI * 2);
 
-            /*
+            
             let vAngle = ((Math.PI * 2) + Math.atan2(player.physics.vy, player.physics.vx)) % (Math.PI * 2);
 
-            if(player.physics.vy**2 + player.physics.vx**2 < 1){
+            if(player.physics.vy**2 + player.physics.vx**2 < 1 || Math.abs(player.physics.vr) < 0.0035){
                 player.physics.action = 0;
             }else if(Math.abs(((Math.PI * 4) + player.physics.r + Math.PI/2) % (Math.PI * 2) - vAngle) < Math.PI/2){
                 player.physics.action = 1;
             }else{
                 player.physics.action = 2;
             }
-            */
-
-            if(Math.abs(player.physics.vr) < 0.01){
-                player.physics.action = 0;
-            }else if(player.physics.vr > 0){
-                player.physics.action = 2;
-            }else{
-                player.physics.action = 1 ;
-            }
-
+            
             if (gameMap.getCurrentTile(player.physics.x, player.physics.y) === "M") {
                 player.physics.x = gameMap.spawnPoint[0];
                 player.physics.y = gameMap.spawnPoint[1]; 
